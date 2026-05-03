c1674a6aae017fe090fb020a29bd0272f197144f
loiiinhoi <loicao992@gmail.com>
2026-05-03 23:50:10 +0700

    update bai4

--- src/main/java/org/example/bai4/FileUtils.java
+++ src/main/java/org/example/bai4/FileUtils.java
@@ -1,8 +1,12 @@
 package org.example.bai4;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtils {
-    // Cố tình code "chết" dấu gạch chéo của Windows
+    // Đã Refactor: Sử dụng thư viện chuẩn để tự động xử lý dấu phân cách
     public static String createLogPath(String folder, String fileName) {
-        return folder + "\\" + fileName;
+        Path path = Paths.get(folder, fileName);
+        return path.toString();
     }
 }
