4d4cb46eaf33c667ef2c3c7633ae96a6b76d7221
loiiinhoi <loicao992@gmail.com>
2026-05-03 23:52:31 +0700

    refactor bai4

--- src/main/java/org/example/bai4/FileUtils.java
+++ src/main/java/org/example/bai4/FileUtils.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileUtils {
-    // Đã Refactor: Sử dụng thư viện chuẩn để tự động xử lý dấu phân cách
+    // Refactor: Sử dụng thư viện chuẩn để tự động xử lý dấu phân cách
     public static String createLogPath(String folder, String fileName) {
         Path path = Paths.get(folder, fileName);
         return path.toString();
